f3e924f58dd3c69eab1ccd89f08cc1adc9328c4e
JUSTLISSS0 <94492161+JUSTLISSS0@users.noreply.github.com>
2021-12-02 23:30:36 +0000

    added random stuff to see what it would do

--- todo.py
+++ todo.py
@@ -1,1 +1,24 @@
-
+myTodo = ["clean my room 5 times#","do some homework 8 times","#play a little bit of video games before i go to bed","go shopping for myself again","fold my clothes 11 times in a row#"]
+print(myTodo)
+print(f'{len(myTodo)} thing(s) left to do')
+print(myTodo.index("clean my room 5 times#"))
+for task in myTodo:
+  print(task)
+myTodo.append("hello")
+for task in myTodo:
+  print(task)
+myTodo.remove ("clean my room 5 times#")
+for task in myTodo:
+  print(task)
+myTodo.sort()
+for task in myTodo:
+   print(task)
+myTodo.insert(2, "im the best")
+for task in myTodo:
+  print(task)
+myTodo.reverse()
+for task in myTodo:
+  print(task)
+myTodo.clear()
+for task in myTodo:
+  print(task)
